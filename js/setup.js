8fa459891765a5f1a5e13e88b877afba00853fcd
John Nash <mihailshtv@gmail.com>
2020-02-05 04:10:55 +0500

    сделал обработчики кликов и нажатий клавиш

--- js/setup.js
+++ js/setup.js
@@ -1,14 +1,66 @@
 'use strict';
 
-var setup = document.querySelector('.setup');
-setup.classList.remove('hidden');
+var ESC_KEY = 'Escape';
+var ENTER_KEY = 'Enter';
 
+var setup = document.querySelector('.setup');
 var similarList = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+var setupSimilar = document.querySelector('.setup-similar');
+var setupOpen = document.querySelector('.setup-open');
+var setupClose = document.querySelector('.setup-close');
+var setupOpenIcon = document.querySelector('.setup-open-icon');
+var setupUserName = document.querySelector('.setup-user-name');
+
+var onPopupEscPress = function (evt) {
+  if (evt.key === ESC_KEY && evt.target !== setupUserName) {
+    closePopup();
+  }
+};
+
+var openPopup = function () {
+  setup.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+var closePopup = function () {
+  setup.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+var onButtonClick = function () {
+  openPopup();
+};
+
+var onCloseButtonClick = function () {
+  closePopup();
+};
+
+var onIconKeydown = function (evt) {
+  if (evt.key === ENTER_KEY) {
+    openPopup();
+  }
+};
+
+var onCloseButtonKeydown = function (evt) {
+  if (evt.key === ENTER_KEY) {
+    closePopup();
+  }
+};
+
 
 var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var COATS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+
+var COATS = [
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'
+];
+
 var EYES = ['black', 'red', 'blue', 'yellow', 'green'];
 var WIZARDS_AMOUNT = 4;
 
@@ -57,6 +109,9 @@ for (var j = 0; j < wizards.length; j++) {
 }
 
 similarList.appendChild(fragment);
-
-var setupSimilar = document.querySelector('.setup-similar');
 setupSimilar.classList.remove('hidden');
+
+setupOpen.addEventListener('click', onButtonClick);
+setupClose.addEventListener('click', onCloseButtonClick);
+setupOpenIcon.addEventListener('keydown', onIconKeydown);
+setupClose.addEventListener('keydown', onCloseButtonKeydown);
